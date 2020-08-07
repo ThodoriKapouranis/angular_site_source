f223676896e43679f4249a72ef9ad2c636ccf5fd
Thodori Kapouranis <51767101+ThodoriKapouranis@users.noreply.github.com>
2020-08-07 14:01:50 -0400

    Add descriptions

--- app/work.service.ts
+++ app/work.service.ts
@@ -37,7 +37,7 @@ export class WorkService {
     '   "description": "Two player checkers created with Angular framework. Will program an AI in the coming weeks or months.",'+
     '   "github_url":"https://github.com/ThodoriKapouranis/Checkers",'+
     '   "url":"https://thodorikapouranis.github.io/Checkers/",'+
-    '    "big_description": "<strong> Overiew </strong> <hr> <p> hello sit amet consectetur adipisicing elit. Quis maiores ex impedit doloremque neque obcaecati facilis libero repellat cupiditate? Natus soluta minima</p> atque! Quaerat reiciendis natus unde amet. Ratione aspernatur, hic qui totam facilis consectetur suscipit dicta. Possimus eligendi, quod aut libero, deserunt ratione distinctio perferendis officia debitis vitae iste. Dicta tenetur harum nulla, tempora ad reprehenderit eligendi alias placeat atque? Eaque sed facilis sequi cum maxime ab. Facere, nihil sed ullam assumenda iusto animi, voluptatem est facilis ad at ratione delectus iure omnis enim eius. Mollitia sint quidem deleniti totam architecto veritatis, impedit eaque veniam eius animi deserunt molestias!" }',
+    '    "big_description": "<strong>ECE-469-1 Artificial Intelligence</strong><hr><p>This project is supposed to be the skeleton for the first project of this class, where we are required to program an AI that uses iterative deepening and an alpha-beta searching, along with a heuristic function.</p>" }',
 
 
     '{"title":"Hashtables, Heaps, Dijkstra",'+
@@ -47,16 +47,16 @@ export class WorkService {
     '   "description": "Hashtables and Heaps coded from scratch in C++, thoroughly tested all test cases to confirm robusticity. Will be used to implement Dijkstras soon.",'+
     '   "github_url":"https://github.com/ThodoriKapouranis/DSA2-Heap",'+
     '   "url":"",'+
-    '    "big_description": "" }',
+    '    "big_description": "<strong>ECE 264 Data Structures & Algorithms</strong> <hr> <p>The prerequesite course for DSA2 is DSA1, where we covered the functioning behind stacks and queues, search trees, and numerous types of sorts and their ideal use conditions. We covered fundamental topics such as big O notation, good programming methodology and C++ programming</p><br><p><strong>ECE-365 Data Structures & Algorithms II</strong></p><hr><p> DSA2 is a class I will be taking in fall 2020, but I decided to finish most of the projects during summer by learning off the textbook. This course includes hashtables, heaps, and popular algorithms like dijkstra, which is a algorithm ive encountered before in Communcation Networks as its used for finding the optimal path across routers.</p>"}',
 
     '{"title":"JLPT Vocab",'+
     ' "context":"Summer 2019",'+
     '   "image":"assets/jlpt.png",'+
     '   "tech":["JavaScript","jQuery","CSS","HTML"],'+
-    '   "description": "Uses a google spreadsheet API to randomly grab a vocabulary word within a certain difficulty (pre-sorted) and quiz the user. Will return to add more functionality, possibily rewriting it with a proper database, because the API is VERY slow.",'+
+    '   "description": "Uses a google spreadsheet API to randomly grab a vocabulary word within a certain difficulty (pre-sorted) and quiz the user. ",'+
     '   "github_url":"https://github.com/ThodoriKapouranis/JLPT-vocab",'+
     '   "url":"https://thodorikapouranis.github.io/JLPT-vocab/",'+
-    '    "big_description": "" }',
+    '    "big_description": "Will return to add more functionality, possibily rewriting it with a proper database, because the API is VERY slow." }',
 
     '{"title":"One Page",'+
     ' "context":"Winter 2019",'+
@@ -85,7 +85,7 @@ export class WorkService {
     '   "description": "Client to Server communication (HTTP) that can store messages from user to sender using SQL, allowing client to client communication. Also done using TOR and inspected with Wireshark to ensure that encryption is working. Client and Server were run on AWS.",'+
     '   "github_url":"https://github.com/ThodoriKapouranis/Terminal-Chatting",'+
     '   "url":"",'+
-    '    "big_description": "" }',
+    '    "big_description": "<strong>ECE-303: Communication Networks</strong><hr> <p>This course covered network protocols, architecture, security, privacy, routing and congestion control, Internet, local area networks, wireless networks, multimedia services. It covered all 5 layers of the internet going indepth in each. We used Wireshark and tcpdump to investigate our traffic and get a real world understanding for how our traffic flows and communicates with servers. We also covered basic cybersecurity topics such as spoofing, SQL injections, and man in the middle attacks. Additionally, we also learnt about VPN workings and made our own VPNs using OVPN and hosting on AWS.</p><br><strong>TOR & VPN</strong><hr><p>The project was initially done with two AWS servers, one for the client and one for the chatting server host. TCP dump was used to be able to monitor the communication between these two terminals and peek into what information is visible to each other.</p> <p>OVPN was used to create a VPN pathway for the chatting terminal so it can use another AWS server client IP to send HTTP requests to the host server. Then, TOR was used to make the communication completely peer to peer, masking the IP of both servers from each other.</p>" }',
 
   ]
 
